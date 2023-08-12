2c660865f22d4cddeb98d90223893f05bb35826d
Ayush Rana <108958206+hazy-aura@users.noreply.github.com>
2023-08-12 10:15:01 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Headers from "./Headers";
+import Footer from "./Footer";
+import Note from "./Note"
+function App() {
+  return (
+    <html>
+      <Headers />
+      <Note />
+      <Footer />
+    </html>
+  );
+}
+
+export default App;
